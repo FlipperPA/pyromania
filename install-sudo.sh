c1e14ce5d4fd6bb5c563e6b78206188300fbc2dc
Allen, Timothy <tallen@wharton.upenn.edu>
2023-02-21 10:12:37 -0500

    Add better comments to installation, detect if already installed.

--- install-sudo.sh
+++ install-sudo.sh
@@ -1,5 +1,17 @@
-echo "To install pyromania for all users on your system, sudo access it required."
+if grep -q "# Start Pyromania" /etc/bashrc; then
+    echo "It appears Pyromania is already installed for all users. Remove it from"
+    echo "/etc/bashrc if you need to reinstall."
+else
+    sudo curl -sS -o /etc/bashrc-pyro.sh https://raw.githubusercontent.com/FlipperPA/pyromania/main/pyro.sh
 
-sudo curl -o /etc/bashrc-pyro.sh https://raw.githubusercontent.com/FlipperPA/pyromania/main/pyro.sh
-sudo echo "# Start Pyromania for all system users." >> /etc/bashrc
-sudo echo "source /etc/bashrc-pyro.sh" >> /etc/bashrc
+    sudo echo "" >> /etc/bashrc
+    sudo echo "# Start Pyromania upon login for venv management." >> /etc/bashrc
+    sudo echo "source /etc/bashrc-pyro.sh" >> /etc/bashrc
+    sudo echo "export VENV_PYTHON=`which python3`" >> /etc/bashrc
+    sudo echo "# End Pyromania configuration." >> /etc/bashrc
+
+    echo "Pyromania has been installed!"
+    echo ""
+
+    pyro
+fi
--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
-curl -o ~/.bashrc-pyro.sh https://raw.githubusercontent.com/FlipperPA/pyromania/main/pyro.sh
-echo "# Start Pyromania upon login for venv management." >> ~/.bashrc
-echo "source /etc/bashrc-pyro.sh" >> /etc/bashrc
+if grep -q "# Start Pyromania" ~/.bashrc; then
+    echo "It appears Pyromania is already installed. Remove it from ~/.bashrc if"
+    echo "you need to reinstall."
+else
+    curl -sS -o ~/.bashrc-pyro.sh https://raw.githubusercontent.com/FlipperPA/pyromania/main/pyro.sh
+
+    echo "" >> ~/.bashrc
+    echo "# Start Pyromania upon login for venv management." >> ~/.bashrc
+    echo "source ~/.bashrc-pyro.sh" >> ~/.bashrc
+    echo "export VENV_PYTHON=`which python3`" >> ~/.bashrc
+    echo "# End Pyromania configuration." >> ~/.bashrc
+
+    echo "Pyromania has been installed!"
+    echo ""
+
+    pyro
+fi
